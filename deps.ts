4c9f724d6b63e1187a5f671b82da41f7eee49125
fredlc <fredlefevre@outlook.com>
2020-07-23 12:06:53 -0400

    Updated certain dependencies with specific version numbers

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
 // Standard Library
 export * as log from 'https://deno.land/std@0.57.0/log/mod.ts';
-export { join } from 'https://deno.land/std/path/mod.ts';
-export { BufReader } from 'https://deno.land/std/io/bufio.ts';
-export { parse } from 'https://deno.land/std/encoding/csv.ts';
+export { join } from 'https://deno.land/std@0.61.0/path/mod.ts';
+export { parse } from 'https://deno.land/std@0.61.0/encoding/csv.ts';
+export { BufReader } from 'https://deno.land/std@0.61.0/io/bufio.ts';
 // Third Party
 export { Application, send } from 'https://deno.land/x/oak@v5.0.0/mod.ts';
 export * as _ from 'https://deno.land/x/lodash@4.17.15-es/lodash.js';
